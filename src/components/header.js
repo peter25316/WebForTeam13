7d3dd501ff3b8858418c269c2406a26ee6f3fbac
phuonggtrann <phuongg.trann8799@gmail.com>
2021-04-18 20:25:37 -0400

    Update footer

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,18 @@
 import '../style/header.css';
 import { Link } from "react-router-dom";
+import discordLogo from "../static/discord_ava.png"
 
 function Header() {
     return (
         <div className="Header">
+            <Link to="/">
+                <img className="header-logo" src={discordLogo}/>
+            </Link>
+                
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/commands">Commands</Link>
-                <Link to="/random">Login</Link>
+                <Link className='header-link' to="/">Home</Link>
+                <Link className='header-link' to="/commands">Commands</Link>
+                <Link className='header-link' to="/random">Login</Link>
             </nav>
         </div>
     )
