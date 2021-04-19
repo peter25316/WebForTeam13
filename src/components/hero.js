4ad3414bc4784dc97beffbe2ab8d2426917f7db3
phuonggtrann <phuongg.trann8799@gmail.com>
2021-04-18 21:30:52 -0400

    Add group members + hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,33 @@
 import '../style/hero.css';
+import discordLogo from "../static/discord_ava.png"
 
 function Hero(){
     return (
         <div className="Hero">
-            This is Hero
+            <div className="hero-text">
+                <h1>
+                    CS321 Team 13 D-STOCK BOT
+                </h1>
+                <h4>
+                    This is a class project of CS321 GMU
+                </h4>
+                <ul>Amy Tran</ul>
+                <ul>Frank Costantino</ul>
+                <ul>Saugat Adhikari</ul>
+                <ul>William Melchor-Guerra</ul>
+                <ul>Phuong Tran</ul>
+                <ul>Peter Huynh</ul>
+                <ul>Olivia Vuong</ul>
+                <a 
+                    className="hero-invite"
+                    href="https://discord.com/"
+                    target="_blank"
+                    >Add to Discord
+                </a>
+            </div>
+            <div>
+                <img className="hero-logo" src={discordLogo}/>
+            </div>
         </div>
     )
 }
